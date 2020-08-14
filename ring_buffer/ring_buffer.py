7af0c2ff88f592ecca648847ca3b3f2ca1e3b403
unknown <prakashbahadursingh2@gmail.com>
2020-08-14 08:32:07 -0700

    RingBuffer class is complete with append and get method

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,37 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.oldest_item = 0
 
     def append(self, item):
-        pass
+        if(len(self.storage) == self.capacity):
+            self.storage[self.oldest_item] = item
+            if(self.oldest_item < self.capacity - 1):
+                self.oldest_item += 1
+            else:
+                self.oldest_item = 0
 
     def get(self):
-        pass
+        return self.storage
+    
+    
+# buffer = RingBuffer(3)
+
+# buffer.get()   # should return []
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# buffer.get()   # should return ['a', 'b', 'c']
+
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+
+# buffer.get()   # should return ['d', 'b', 'c']
+
+# buffer.append('e')
+# buffer.append('f')
+
+# buffer.get()   # should return ['d', 'e', 'f']
